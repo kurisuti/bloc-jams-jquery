a3de164b476d4b1c4ac56fda161a706e5dfbe53f
christie chang <kurisuti.cc@gmail.com>
2019-10-31 10:59:31 -0700

    checkpoint-17 part 1

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -14,9 +14,9 @@
       </tr>
     `);
 
-    song.element.on("click" , event => {
+    song.element.on("click",event => {
       player.playPause(song);
-      $("button"#play-pause").attr("playState",player.playState);
+      $("button#play-pause").attr("playState",player.playState);
     });
 
     $("#song-list").append(song.element);
