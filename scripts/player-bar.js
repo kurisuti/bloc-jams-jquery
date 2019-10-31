c7237371818a7a14e7794cdfa36765168d2413f4
christie chang <kurisuti.cc@gmail.com>
2019-10-31 11:54:45 -0700

    still working on checkpoint-17-previousbutton

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -8,10 +8,21 @@
   if (player.playState !== "playing") {return;}
 
   const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-  const nextSongIndex = currentSongIndex + 1
+  const nextSongIndex = currentSongIndex + 1;
   if (nextSongIndex >= album.songs.length) {return;}
-  
+
   const nextSong = album.songs[nextSongIndex];
   player.playPause(nextSong);
+  });
+
+  $("button#previous").on("click", function(){
+      if (player.playState !== "playing") {return;}
+
+  const previousSongIndex = currentSongIndex - 1;
+  if (previousSongIndex >= album.songs.length) {return;}
+
+
+  const previousSong = album.songs[previousSongIndex];
+  player.playPause(previousSong);
   });
 }
