86f070c7bec811e4fae98d2247a44add7ed93253
christie chang <kurisuti.cc@gmail.com>
2019-10-31 10:47:35 -0700

    still working on checkpoint-17

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,5 +1,12 @@
 {
   $("button#play-pause").on("click",function(){
     player.playPause();
+    $(this).attr("playState",player.playState);
   });
+
+  // $("button#next").on("click",function(){
+  // if (player.playState !== "playing") {return;}
+
+  // const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+  // });
 }
--- scripts/song-list.js
+++ scripts/song-list.js
@@ -1,13 +1,24 @@
 {
-  album.song.forEach( (song,index) => {
-    song.element = $(`
+  album.songs.forEach((song, index) => {
+    song.element =$(`
       <tr>
-        <td>${index + 1}</td>
+        <td>
+            <button>
+              <span class="song-number">${index + 1}</span>
+              <span class="ion-play"></span>
+              <span class="ion-pause"></span>
+            </button>
+        </td>
         <td>${song.title}</td>
         <td>${song.duration}</td>
       </tr>
     `);
 
-      $("#song-list").append(song.element);
+    song.element.on("click" , event => {
+      player.playPause(song);
+      $("button"#play-pause").attr("playState",player.playState);
+    });
+
+    $("#song-list").append(song.element);
   });
 }
